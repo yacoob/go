9b7dbb40feb45e1d4ec8c35feb063d6d385bc326
Jakub Turski <yacoob@gmail.com>
2010-04-17 12:12:00 +0100

    Refactoring.
    * Uh, when I say 'split', I mean 'split'.

--- go/go.py
+++ go/go.py
@@ -4,7 +4,9 @@
 import optparse, sys, threading, time, urllib;
 import bottle;
 
+import redirector;
 import sqldict;
+import trampoline;
 
 
 @bottle.route('/')
@@ -21,152 +23,19 @@ def go_edit_that(shortcut):
 
 @bottle.route('/(?P<shortcut>[^&?/*]+)')
 def go_there(shortcut):
-    db = app['shortcuts.db'];
-
-    if (db.has_key(shortcut)):
-        # if redirect already exist, just go there
-        bottle.redirect(db[shortcut]);
-    else:
-        # if it doesn't, redirect to prefilled edit form
-        url = '/and/add?' + urllib.urlencode({'short': shortcut});
-        bottle.redirect(url);
+    return redirector.handle_shortcut(app['shortcuts.db'], shortcut);
 
 
 @bottle.route('/and/(?P<cmd>.*)')
 def go_command(cmd):
-    args = {};
-    # FIXME: sanitize the params here
-    args['short'] = bottle.request.GET.get('short', '');
-    args['long'] = bottle.request.GET.get('long', '');
-    db = app['shortcuts.db'];
-
-    if (cmd == 'add'):
-        # add new redirect
-        args['message'] = 'Add a new shortcut:';
-        args['title'] = '- add a new shortcut';
-        if args['short'] and args['long']:
-            # if both short and long name are provided, add it straight away
-            db[args['short']] = args['long'];
-            bottle.redirect('/and/list');
-        else:
-            # otherwise, present user with edit form
-            return bottle.template('go_edit', **args);
-
-    elif (cmd == 'edit'):
-        if args['short']:
-            # edit a redirect
-            if db.has_key(args['short']):
-                args['message'] = 'Edit:';
-                args['long'] = db[args['short']];
-                args['title'] = '- edit a shortcut';
-            else:
-                args['message'] = 'Add a new shortcut:';
-                args['title'] = '- add a new shortcut';
-            return bottle.template('go_edit', **args);
-        else:
-            # redirect to / if no name was supplied
-            bottle.redirect('/and/list');
-
-    elif (cmd == 'del'):
-        # delete a redirect
-        if args['short'] and db.has_key(args['short']):
-            # if it exist, remove it and redirect to edit page, as a last
-            # chance to save this shortcut
-            args['message'] = 'Old shortcut removed, but you can always add it back here:';
-            args['long'] = db[args['short']];
-            args['title'] = '- last chance to save a shortcut!';
-            del db[args['short']];
-            return bottle.template('go_edit', **args);
-        else:
-            url = '/and/add?' + urllib.urlencode({'short': args['short']});
-            bottle.redirect(url);
-
-    elif (cmd == 'list'):
-        # show list of all redirects
-        shortcuts = db.items();
-        shortcuts.sort();
-        args = {
-          'list':  shortcuts,
-          'title': '- shortcuts list',
-        };
-        return bottle.template('go_list', **args);
-
-    else:
-        # default endpoint
-        bottle.redirect('/and/list');
+    return redirector.handle_command(app['shortcuts.db'], cmd);
 
 
 @bottle.route('/hop/(?P<cmd>.*)')
 def hop_command(cmd):
-    args = {};
-    # FIXME: sanitize the params here
-    args['url'] = bottle.request.GET.get('url', '');
-    args['id'] = bottle.request.GET.get('id', '');
-    db = app['trampolina.db'];
-    db_old = app['trampolina_old.db'];
-
-    if (cmd == 'push'):
-        if (args['url']):
-            app['timestamp_lock'].acquire();
-            db[str(time.time())] = args['url'];
-            app['timestamp_lock'].release();
-            args['title'] = '- trampolina push succeeded';
-            return bottle.template('hop_msg', **args);
-        else:
-            bottle.redirect('/hop/list');
-
-    elif (cmd == 'pop'):
-        urls_keys = db.keys();
-        urls_keys.sort();
-        latest_id = urls_keys[-1] if len(urls_keys) else 0;
-        id = args['id'] if args['id'] else latest_id;
-        if (id not in urls_keys):
-            bottle.redirect('/hop/list');
-
-        url = db[id];
-        del db[id];
-
-        # FIXME: purge old urls from db_old
-        db_old[id] = url;
-
-        bottle.redirect(url);
-
-    elif (cmd == 'list'):
-        urls = db.items();
-        urls.sort(reverse=True);
-        old_urls = db_old.items();
-        old_urls.sort(reverse=True);
-        args = {
-            'stack':  urls,
-            'viewed': old_urls,
-            'title':  '- trampolina URLs list',
-        };
-        return bottle.template('hop_list', **args);
-
-    elif (cmd == 'rss'):
-        urls = db.items();
-        urls.sort(reverse=True);
-        describe = lambda item: {
-            'url': item[1],
-            'timestamp': item[0],
-            'datetime': time.ctime(float(item[0])),
-        };
-        base_url = 'http://' + app['host'] + ':' + str(app['port']);
-        stack = map(describe, urls);
-        args = {
-           'stack': stack,
-           'title': '- new trampolina URLs',
-           'description': 'New URLs in trampolina',
-           'timestamp': time.ctime(),
-           'list_url': base_url + '/hop/list',
-           'pop_url': base_url + '/hop/pop?id=',
-        };
-        bottle.response.content_type = 'text/xml';
-        return bottle.template('hop_rss', **args);
-
-    else:
-        # default endpoint
-        bottle.redirect('/hop/list');
+    return trampoline.handle_command(
+        app['trampolina.db'], app['trampolina_old.db'], cmd, app['timestamp_lock']
+    );
 
 
 @bottle.route('/static/:filename')
