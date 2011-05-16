b5075129a42817bcc3479684ae7a1b6f380c52a3
Jakub Turski <yacoob@gmail.com>
2011-05-16 23:34:47 +0100

    React to clicks on list items.

--- android/src/org/yacoob/UrlList.java
+++ android/src/org/yacoob/UrlList.java
@@ -2,7 +2,11 @@ package org.yacoob;
 
 import android.app.ListActivity;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.view.View;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class UrlList extends ListActivity {
 
@@ -12,5 +16,10 @@ public class UrlList extends ListActivity {
         setContentView(R.layout.main);
         setListAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,
                                                 getResources().getStringArray(R.array.url_list)));
+    }
+
+    @Override
+    public void onListItemClick(ListView l, View v, int position, long id) {
+        Toast.makeText(this, ((TextView) v).getText(), 1500).show();
     }
 }
