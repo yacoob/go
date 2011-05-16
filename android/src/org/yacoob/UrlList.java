d8fd3ab3ce74935ef880c0f24dff6d6d10c91471
Jakub Turski <yacoob@gmail.com>
2011-05-16 23:49:26 +0100

    Actually open the URL.

--- android/src/org/yacoob/UrlList.java
+++ android/src/org/yacoob/UrlList.java
@@ -1,25 +1,30 @@
 package org.yacoob;
 
 import android.app.ListActivity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.view.View;
-import android.os.Bundle;
-import android.widget.TextView;
 import android.widget.Toast;
 
 public class UrlList extends ListActivity {
+    private String[] url_list;
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        setListAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,
-                                                getResources().getStringArray(R.array.url_list)));
+        url_list = getResources().getStringArray(R.array.url_list);
+        setListAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, url_list));
     }
 
     @Override
     public void onListItemClick(ListView l, View v, int position, long id) {
-        Toast.makeText(this, ((TextView) v).getText(), 1500).show();
+        String url = url_list[position];
+        Toast.makeText(this, url, 1500).show();
+        this.startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(url)));
     }
 }
