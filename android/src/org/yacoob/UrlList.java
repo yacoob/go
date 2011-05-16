013ea2a4f674f1806d1165f66ea638527f9870cb
Jakub Turski <yacoob@gmail.com>
2011-05-16 23:08:02 +0100

    It's a list now!
    * Use ListActivity instead of normal activity.
    * Ignore bin/ directory.
    * Some IntelliJ configuration.

--- android/src/org/yacoob/UrlList.java
+++ android/src/org/yacoob/UrlList.java
@@ -1,15 +1,16 @@
 package org.yacoob;
 
-import android.app.Activity;
+import android.app.ListActivity;
+import android.widget.ArrayAdapter;
 import android.os.Bundle;
 
-public class UrlList extends Activity
-{
-    /** Called when the activity is first created. */
+public class UrlList extends ListActivity {
+
     @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        setListAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,
+                                                getResources().getStringArray(R.array.url_list)));
     }
 }
