cc74a33484c593d2ecee3c5e9a5c5943c0ad518a
yacoob@gmail.com <yacoob@gmail.com>
2011-05-28 22:24:27 +0100

    Now slightly more complex.
    * inner class for fetching URLs;
    * refreshing list using AsyncTask.

--- android/src/org/yacoob/UrlList.java
+++ android/src/org/yacoob/UrlList.java
@@ -3,6 +3,7 @@ package org.yacoob;
 import android.app.ListActivity;
 import android.content.Intent;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
@@ -14,14 +15,55 @@ import org.apache.http.impl.client.DefaultHttpClient;
 
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 
 public class UrlList extends ListActivity {
     private static String list_url = "http://go/hop/list?json=1";
     private String[] url_list;
-    private DefaultHttpClient f = new DefaultHttpClient();
-    private HttpGet r = new HttpGet(list_url);
+
+    private static class UrlFetch {
+        private static DefaultHttpClient f = new DefaultHttpClient();
+
+        private static String fetchUrl(String url) {
+            BufferedReader reader = null;
+            StringBuilder sb = new StringBuilder();
+            HttpGet r = new HttpGet(url);
+            try {
+                HttpResponse l = f.execute(r);
+                reader = new BufferedReader(new InputStreamReader(l.getEntity().getContent()));
+
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    sb.append(line).append("\n");
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            finally {
+                try {
+                    if (reader != null) {
+                        reader.close();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return sb.toString();
+        }
+    }
+
+    private class RefreshList extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... params) {
+            return UrlFetch.fetchUrl(params[0]);
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            Toast.makeText(getApplicationContext(), result, 30000).show();
+        }
+    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -29,8 +71,7 @@ public class UrlList extends ListActivity {
         setContentView(R.layout.main);
         url_list = getResources().getStringArray(R.array.url_list);
         setListAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, url_list));
-        // TODO:Use AsyncTask for this and future updates
-        refreshListFromUrl();
+        new RefreshList().execute(list_url);
     }
 
     @Override
@@ -39,39 +80,4 @@ public class UrlList extends ListActivity {
         Toast.makeText(this, url, 1500).show();
         this.startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(url)));
     }
-
-    private void refreshListFromUrl() {
-        try {
-            HttpResponse l = f.execute(r);
-            InputStream is = l.getEntity().getContent();
-            // TODO: actually use fetched data
-            Toast.makeText(this, convertStreamToString(is), 30000).show();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private static String convertStreamToString(InputStream is) {
-        // TODO: replace with private class converting directly to String[]
-        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
-        StringBuilder sb = new StringBuilder();
-
-        String line;
-        try {
-            while ((line = reader.readLine()) != null) {
-                sb.append(line).append("\n");
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        finally {
-            try {
-                is.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-        return sb.toString();
-    }
-
 }
