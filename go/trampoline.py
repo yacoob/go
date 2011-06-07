b5859f559318b5139046810f8e0cd75dfce80ebc
Jakub Turski <yacoob@gmail.com>
2011-06-08 00:42:10 +0100

    Refactoring: split trampoline.handle_command.
    * It's still ugly, but at least you can eat it in chunks.

--- go/trampoline.py
+++ go/trampoline.py
@@ -3,7 +3,6 @@
 
 import json, urlparse, time;
 
-
 def describe_urls(db, json=False):
     entries = db.items();
     entries.sort(reverse=True);
@@ -18,82 +17,102 @@ def describe_urls(db, json=False):
         return descriptive;
 
 
-def handle_command(app, cmd, params):
-    args = {};
-    args.update(params);
+def push_url(app, args):
+    if (args.has_key('url') and args['url']):
+        db = app['hop.db'];
+        app['timestamp_lock'].acquire();
+        db[str(time.time())] = args['url'];
+        app['timestamp_lock'].release();
+        args['title'] = '- trampoline push succeeded';
+        return { 'action': 'template', 'template_name': 'hop_msg',
+                 'template_args': args };
+    else:
+        return { 'action': 'redir', 'url': '/hop/list' };
+
+
+def pop_url(app, args):
     db = app['hop.db'];
     db_old = app['hop_old.db'];
-
-    if (cmd == 'push'):
-        if (args.has_key('url') and args['url']):
-            app['timestamp_lock'].acquire();
-            db[str(time.time())] = args['url'];
-            app['timestamp_lock'].release();
-            args['title'] = '- trampoline push succeeded';
-            return { 'action': 'template', 'template_name': 'hop_msg',
-                     'template_args': args };
-        else:
-            return { 'action': 'redir', 'url': '/hop/list' };
-
-    elif (cmd == 'pop'):
-        urls_keys = db.keys();
-        urls_keys.sort();
-        latest_id = urls_keys[-1] if len(urls_keys) else 0;
-        id = args['id'] if (args.has_key('id') and args['id']) else latest_id;
-        if (id not in urls_keys):
-            return { 'action': 'redir', 'url': '/hop/list' };
-
-        url = db[id];
-        del db[id];
-
-        # FIXME: purge old urls from db_old
-        db_old[id] = url;
-
-        return { 'action': 'redir', 'url': url };
-
-    elif (cmd == 'list'):
-        stack = describe_urls(db);
-        viewed = describe_urls(db_old);
-        if (args['json']):
-            args = {
-                'jsonized': json.dumps({'stack': describe_urls(db, True),
-                                        'viewed': describe_urls(db_old, True)})
-                };
-            template = 'json';
-        else:
-            args = {
-                'stack':  describe_urls(db),
-                'viewed': describe_urls(db_old),
-                'title':  '- trampoline URLs list',
-                };
-            template = 'hop_list';
-
-        return { 'action': 'template', 'template_name': template,
-                 'template_args': args };
-
-    elif (cmd == 'rss'):
-        urls = db.items();
-        urls.sort(reverse=True);
-        describe = lambda item: {
-            'url': item[1],
-            'timestamp': item[0],
-            'datetime': time.ctime(float(item[0])),
-        };
-        # Try to reconstruct base_url from requested one.
-        parsed_req_url = urlparse.urlparse(args['requested_url']);
-        base_url = urlparse.urlunparse(parsed_req_url[0:2] + ('', '', '', ''));
-        stack = map(describe, urls);
-        args = {
-           'stack': stack,
-           'title': '- new trampoline URLs',
-           'description': 'New URLs on trampoline',
-           'timestamp': time.ctime(),
-           'list_url': base_url + '/hop/list',
-           'pop_url': base_url + '/hop/pop?id=',
-        };
-        return { 'action': 'template', 'template_name': 'hop_rss',
-                 'template_args': args, 'content_type': 'text/xml' };
-
-    else:
-        # default endpoint
+    urls_keys = db.keys();
+    urls_keys.sort();
+    latest_id = urls_keys[-1] if len(urls_keys) else 0;
+    id = args['id'] if (args.has_key('id') and args['id']) else latest_id;
+    if (id not in urls_keys):
         return { 'action': 'redir', 'url': '/hop/list' };
+
+    url = db[id];
+    del db[id];
+
+    # FIXME: purge old urls from db_old
+    db_old[id] = url;
+
+    return { 'action': 'redir', 'url': url };
+
+
+def list_urls(app, args):
+    db = app['hop.db'];
+    db_old = app['hop_old.db'];
+    if (args['json']):
+        args = {
+            'jsonized': json.dumps({'stack': describe_urls(db, True),
+                                    'viewed': describe_urls(db_old, True)})
+            };
+        template = 'json';
+    else:
+        args = {
+            'stack':  describe_urls(db),
+            'viewed': describe_urls(db_old),
+            'title':  '- trampoline URLs list',
+            };
+        template = 'hop_list';
+
+    return { 'action': 'template', 'template_name': template,
+             'template_args': args };
+
+
+def rss(app, args):
+    db = app['hop.db'];
+    urls = db.items();
+    urls.sort(reverse=True);
+    describe = lambda item: {
+        'url': item[1],
+        'timestamp': item[0],
+        'datetime': time.ctime(float(item[0])),
+    };
+    # Try to reconstruct base_url from requested one.
+    parsed_req_url = urlparse.urlparse(args['requested_url']);
+    base_url = urlparse.urlunparse(parsed_req_url[0:2] + ('', '', '', ''));
+    stack = map(describe, urls);
+    args = {
+       'stack': stack,
+       'title': '- new trampoline URLs',
+       'description': 'New URLs on trampoline',
+       'timestamp': time.ctime(),
+       'list_url': base_url + '/hop/list',
+       'pop_url': base_url + '/hop/pop?id=',
+    };
+    return { 'action': 'template', 'template_name': 'hop_rss',
+             'template_args': args, 'content_type': 'text/xml' };
+
+
+def rest(app, args):
+    return { 'action': 'redir', 'url': '/hop/list' };
+
+
+def handle_command(app, cmd, params):
+
+    cmd_map = { 'push': push_url,
+                'pop':  pop_url,
+                'list': list_urls,
+                'rss':  rss,
+                'rest': rest };
+
+    args = {};
+    args.update(params);
+
+    if cmd_map.has_key(cmd):
+        result = cmd_map[cmd](app, args);
+    else:
+        result = cmd_map['list'](app, args);
+
+    return result;
